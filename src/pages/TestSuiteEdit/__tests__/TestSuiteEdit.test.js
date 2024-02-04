bf30472d18a587549d5921a34ce678447c71223a
danielreis00 <danielreis00@gmail.com>
2024-02-04 12:09:19 +0000

    add test skeleton for TestSuiteForm component

--- src/pages/TestSuiteEdit/__tests__/TestSuiteEdit.test.js
+++
@@ -1,4 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TestSuiteEdit from '../TestSuiteEdit';
-
-test('renders TestSuiteEdit', () => {});
---
+++ src/pages/TestSuiteEdit/__tests__/TestSuiteForm.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import TestSuiteForm from '../TestSuiteForm';
+
+test('renders form for a given test suite', () => {});
+
+test('allows changing values of every editable field', () => {});
+
+test('allows to add a new test plan when clicking on add plan button', () => {});
+
+test('allows delete a plan from the plan list', () => {});
+
+test('allows to cancel the edit clicking on cancel button', () => {});
+
+test('when clicking on save it prints the serialized test suite', () => {});
